eb99b8b73f3cc0bc97e97d4a4196bcd49ae3d9c5
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-21 01:24:12 +0000

    add_cart

--- Gemfile
+++ Gemfile
@@ -48,11 +48,15 @@ gem 'dotenv-rails'
 # socialiizatioinを使用できる
 gem 'socialization'
 
- # ページネーションを実装できる
- gem 'kaminari'
+# ページネーションを実装できる
+gem 'kaminari'
 
- # それらしい値を自動生成できる
- gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :tag => 'v2.19.0'
+# それらしい値を自動生成できる
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :tag => 'v2.19.0'
+
+ # ショッピングカートを実装できる
+gem 'acts_as_shopping_cart'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
@@ -17,6 +18,9 @@ Rails.application.routes.draw do
 
   resource :users, only: [:edit, :update] do
     collection do
+      get "cart", :to => "shopping_carts#index"
+      post "cart/create", :to => "shopping_carts#create"
+      delete "cart", :to => "shopping_carts#destroy"
       get "mypage", :to => "users#mypage"
       get "mypage/edit", :to => "users#edit"
       get "mypage/address/edit", :to => "users#edit_address"
