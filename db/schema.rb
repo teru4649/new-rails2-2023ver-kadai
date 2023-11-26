4c6fc079a3930ed3d082d23ab24ae233ada7be47
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-26 05:53:55 +0000

    fin3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_26_012730) do
+ActiveRecord::Schema.define(version: 2023_11_26_054339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2023_11_26_012730) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "recommended_flag", default: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
