2079bb03c90b880ee2ef081e3e5de58d07d3f298
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-26 01:08:16 +0000

    No5

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
+  belongs_to :major_category
   has_many :products, dependent: :destroy
-  
+
   extend DisplayList
   scope :major_categories, -> { pluck(:major_category_name).uniq }
   scope :request_category, -> (category) { find(category.to_i) }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_22_092507) do
+ActiveRecord::Schema.define(version: 2023_11_26_005908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2023_11_22_092507) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "major_category_id"
+    t.index ["major_category_id"], name: "index_categories_on_major_category_id"
   end
 
   create_table "follows", force: :cascade do |t|
@@ -54,6 +56,13 @@ ActiveRecord::Schema.define(version: 2023_11_22_092507) do
     t.datetime "created_at"
     t.index ["likeable_id", "likeable_type"], name: "fk_likeables"
     t.index ["liker_id", "liker_type"], name: "fk_likes"
+  end
+
+  create_table "major_categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "mentions", force: :cascade do |t|
@@ -127,6 +136,7 @@ ActiveRecord::Schema.define(version: 2023_11_22_092507) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "major_categories"
   add_foreign_key "products", "categories"
   add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
