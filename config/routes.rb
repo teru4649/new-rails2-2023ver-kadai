880bf0e7abd0fa1e421a4efb36a1b261b72eb7d3
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-26 00:34:25 +0000

    No.3

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
      :sessions => 'admins/sessions'
    }
  
-   devise_scope :admin do
-     get "dashboard", :to => "dashboard#index"
-     get "dashboard/login", :to => "admins/sessions#new"
-     post "dashboard/login", :to => "admins/sessions#create"
-     delete "dashboard/logout", :to => "admins/sessions#destroy"
-   end
+  devise_scope :admin do
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    delete "dashboard/logout", :to => "admins/sessions#destroy"
+  end
+
+  namespace :dashboard do
+    resources :categories, except: [:new]
+  end
   
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
