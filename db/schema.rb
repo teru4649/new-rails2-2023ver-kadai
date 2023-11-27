45c77daac6b243b52fc51b7f78f0715b3bd2f30e
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-27 08:21:04 +0000

    fin_chapter_22

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_27_063636) do
+ActiveRecord::Schema.define(version: 2023_11_27_082018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2023_11_27_063636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "deleted_flg", default: false, null: false
+    t.string "token", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
